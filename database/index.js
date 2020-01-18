a13aedb229ef5031ca276ab422e41701d0be4183
JohnCharamonde <john.charamonde@gmail.com>
2020-01-17 19:13:07 -0500

    Dev dependencies installed + schema created

--- database/index.js
+++ database/index.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+//new code
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  let repoSchema = mongoose.Schema({
+    id: Number,
+    name: String,
+    owner: {
+      login: String
+    },
+    url: String,
+    forks: Number
+  });
 });
+//end new code
 
 let Repo = mongoose.model('Repo', repoSchema);
 
