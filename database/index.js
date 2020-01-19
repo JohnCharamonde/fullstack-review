aa30477ff8207a8d00c8bc2c57d04067e4770778
JohnCharamonde <john.charamonde@gmail.com>
2020-01-18 19:46:41 -0500

    post not checking for duplicates

--- database/index.js
+++ database/index.js
@@ -19,24 +19,28 @@ let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (repos) => {
   for (let i = 0; i < repos.length; i++) {
-    let temp = new Repo({
-      id: repos[i].id,
-      name: repos[i].name,
-      owner: repos[i].owner.login,
-      url: repos[i].url,
-      forks: repos[i].forks
-    })
-    temp.save((err, tempRepo) => {
-      if (err) return console.log(err);
-      console.log('created a document instance!')
+    Repo.find({ id: repos[i].id }, (err, result) => {
+      if (err) {
+        console.log('err')
+      } else {
+        if (result.length === 0) {
+          let temp = new Repo({
+            id: repos[i].id,
+            name: repos[i].name,
+            owner: repos[i].owner.login,
+            url: repos[i].url,
+            forks: repos[i].forks
+          })
+          temp.save((err, tempRepo) => {
+            if (err) return console.log(err);
+            console.log('created a document instance!');
+          })
+        } else {
+          console.log('You just tried to add a duplicate!');
+        }
+      }
     })
   }
-
-  // tempRepo.save((err, tempRepo) => {
-  //   if(err) return console.log(err);
-  //   console.log('created a document instance!')
-  // })
-  // })
 }
 
 module.exports.save = save;
